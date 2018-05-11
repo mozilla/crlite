b158200eb38b244c668dbd92e0afc75d02fd3454
groovecoder <luke.crouch@gmail.com>
2018-05-11 15:10:19 -0500

    get settings from environment variables

--- get_CRL_revocations/settings.py
+++ get_CRL_revocations/settings.py
@@ -1,7 +1,16 @@
-CT_FETCH_DATA_DIR = '/Users/lcrouch/ct'
-CRL_SERVERS_FILENAME = 'CRL_servers.txt'
-CERTS_OUTFILE = 'certs_using_CRL.json'
-ALL_CRLS_DIR = 'all_CRLs'
-COMBINED_CRL_OUTFILE = 'mega_CRL.json'
-FINAL_REVOKED_CERTS_FILE = 'final_crl_revoked.json'
-FINAL_NONREVOKED_CERTS_FILE = 'final_crl_nonrevoked.json'
+from decouple import config
+
+
+CT_FETCH_DATA_DIR = config('CT_FETCH_DATA_DIR', default='/Users/lcrouch/ct')
+CRL_SERVERS_FILENAME = config(
+    'CRL_SERVERS_FILENAME', default='CRL_servers.txt'
+)
+CERTS_OUTFILE = config('CERTS_OUTFILE', default='certs_using_CRL.json')
+ALL_CRLS_DIR = config('ALL_CRLS_DIR', default='all_CRLs')
+COMBINED_CRL_OUTFILE = config('COMBINED_CURL_OUTFILE', default='mega_CRL.json')
+FINAL_REVOKED_CERTS_FILE = config(
+    'FINAL_REVOKED_CERTS_FILE', default='final_crl_revoked.json'
+)
+FINAL_NONREVOKED_CERTS_FILE = config(
+    'FINAL_NONREVOKED_CERTS_FILE', default='final_crl_nonrevoked.json'
+)
