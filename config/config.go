9c0c036eae30dc1bea072276e4926983e0bba1d9
J.C. Jones <jc@mozilla.com>
2018-09-09 17:49:23 -0700

    Change away from iniflags

--- config/config.go
+++ config/config.go
@@ -6,7 +6,9 @@ package config
 
 import (
 	"flag"
-	"github.com/vharitonsky/iniflags"
+	"fmt"
+	"gopkg.in/ini.v1"
+	"log"
 )
 
 type CTConfig struct {
@@ -21,27 +23,52 @@ type CTConfig struct {
 	IssuerCNFilter    *string
 	LogExpiredEntries *bool
 	AWSS3Bucket       *string
+	Config            *string
 }
 
 func NewCTConfig() *CTConfig {
 	ret := &CTConfig{
-		LogUrlList:        flag.String("logList", "", "URLs of the CT Logs, comma delimited"),
 		Verbose:           flag.Bool("v", false, "Give verbose output"),
-		CertPath:          flag.String("certPath", "", "Path under which to store full DER-encoded certificates"),
 		Offset:            flag.Uint64("offset", 0, "offset from the beginning"),
 		Limit:             flag.Uint64("limit", 0, "limit processing to this many entries"),
-		NumThreads:        flag.Int("numThreads", 1, "Use this many threads per CPU"),
-		RunForever:        flag.Bool("forever", false, "Run forever"),
-		PollingDelay:      flag.Int("pollingDelay", 10, "Wait this many minutes between polls"),
-		IssuerCNFilter:    flag.String("issuerCNList", "", "Prefixes to match for CNs for permitted issuers, comma delimited"),
-		LogExpiredEntries: flag.Bool("logExpiredEntries", false, "Add expired entries to the database"),
-		AWSS3Bucket:         flag.String("awsS3Bucket", "", "AWS S3 Path"),
+		AWSS3Bucket:       flag.String("awsS3Bucket", "", "AWS S3 Path"),
+		Config:            flag.String("config", "~/.ct-fetch.ini", "configuration .ini file"),
+		LogUrlList:        new(string),
+		NumThreads:        new(int),
+		LogExpiredEntries: new(bool),
+		RunForever:        new(bool),
+		PollingDelay:      new(int),
+		IssuerCNFilter:    new(string),
+		CertPath:          new(string),
+	}
+	flag.Parse()
+
+	cfg, err := ini.Load(*ret.Config)
+	if err == nil {
+		log.Printf("Loaded config file from %s", *ret.Config)
+		*ret.LogUrlList = cfg.Section("").Key("logList").String()
+		*ret.NumThreads = cfg.Section("").Key("numThreads").MustInt(1)
+		*ret.LogExpiredEntries = cfg.Section("").Key("logExpiredEntries").MustBool(false)
+		*ret.RunForever = cfg.Section("").Key("runForever").MustBool(false)
+		*ret.PollingDelay = cfg.Section("").Key("pollingDelay").MustInt(10)
+		*ret.IssuerCNFilter = cfg.Section("").Key("issuerCNFilter").String()
+		*ret.CertPath = cfg.Section("").Key("certPath").String()
 	}
 
-	iniflags.Parse()
 	return ret
 }
 
 func (c *CTConfig) Usage() {
 	flag.Usage()
+
+	fmt.Printf("")
+	fmt.Printf("Config file directives:")
+	fmt.Printf("")
+	fmt.Printf("certPath = Path under which to store full DER-encoded certificates")
+	fmt.Printf("issuerCNFilter = Prefixes to match for CNs for permitted issuers, comma delimited")
+	fmt.Printf("runForever = Run forever, pausing `pollingDelay` between runs")
+	fmt.Printf("pollingDelay = Wait this many minutes between polls")
+	fmt.Printf("logExpiredEntries = Add expired entries to the database")
+	fmt.Printf("numThreads = Use this many threads per CPU")
+	fmt.Printf("logList = URLs of the CT Logs, comma delimited")
 }
--- go.mod
+++ go.mod
@@ -6,9 +6,10 @@ require (
 	github.com/golang/protobuf v1.0.0
 	github.com/google/certificate-transparency-go v1.0.12
 	github.com/jcjones/ct-sql v0.0.0-20170221230105-a1a22e4a3ef6
+	github.com/jcjones/go-progressdisplay v0.0.0-20180910000447-19ad88f5fde4 // indirect
 	github.com/jmespath/go-jmespath v0.0.0-20160202185014-0b12d6b521d8
 	github.com/jpillora/backoff v0.0.0-20170918002102-8eab2debe79d
-	github.com/vharitonsky/iniflags v0.0.0-20180513140207-a33cd0b5f3de
 	golang.org/x/crypto v0.0.0-20180314180259-21652f85b0fd
 	golang.org/x/net v0.0.0-20180314180242-e0c57d8f86c1
+	gopkg.in/ini.v1 v1.38.2
 )
