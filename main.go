916fa22ee7ade65022227ac2ae0a8e00a0311508
J.C. Jones <jc@mozilla.com>
2019-08-29 13:42:23 -0700

    Catch setting up both local disk and firestore

--- main.go
+++ main.go
@@ -362,14 +362,24 @@ func (lw *LogWorker) downloadCTRangeToChannel(entryChan chan<- CtLogEntry) (uint
 func main() {
 	var err error
 	var storageDB storage.CertDatabase
-	if ctconfig.CertPath != nil && len(*ctconfig.CertPath) > 0 {
+
+	hasLocalDiskConfig := ctconfig.CertPath != nil && len(*ctconfig.CertPath) > 0
+	hasFirestoreConfig := ctconfig.FirestoreProjectId != nil && len(*ctconfig.FirestoreProjectId) > 0
+
+	if hasLocalDiskConfig && hasFirestoreConfig {
+		glog.Fatal("Local Disk and Firestore configurations both found. Exiting.")
+	}
+
+	if hasLocalDiskConfig {
 		backend := storage.NewLocalDiskBackend(0644, *ctconfig.CertPath)
 		glog.Infof("Saving to disk at %s", *ctconfig.CertPath)
 		storageDB, err = storage.NewFilesystemDatabase(*ctconfig.CacheSize, backend)
 		if err != nil {
 			glog.Fatalf("unable to open Certificate Path: %+v: %+v", ctconfig.CertPath, err)
 		}
-	} else if ctconfig.FirestoreProjectId != nil && len(*ctconfig.FirestoreProjectId) > 0 {
+	}
+
+	if hasFirestoreConfig {
 		ctx := context.Background()
 
 		backend, err := storage.NewFirestoreBackend(ctx, *ctconfig.FirestoreProjectId)
