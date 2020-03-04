f239cc6b29ad81313f9f64bc965215607c7106a9
J.C. Jones <jc@mozilla.com>
2020-03-04 14:41:11 -0700

    Rework the key/caches to be binary-packed and always be constructed

--- create_filter_cascade/certs_to_crlite.py
+++ create_filter_cascade/certs_to_crlite.py
@@ -13,6 +13,7 @@ import math
 import os
 import stopwatch
 import sys
+import struct
 from filtercascade import FilterCascade
 
 # Structure of the stats object:
@@ -47,10 +48,35 @@ from filtercascade import FilterCascade
 # }
 
 sw = stopwatch.StopWatch()
+log = logging.getLogger('cert_to_crlite')
+
+
+class CertId(object):
+    def __init__(self, issuerSpkiHash, serial):
+        if not isinstance(issuerSpkiHash, bytes):
+            raise Exception("issuerSpkiHash must be bytes")
+
+        if not isinstance(serial, bytes):
+            raise Exception("serial must be bytes")
+
+        self.issuerSpkiHash = issuerSpkiHash
+        self.serial = serial
+
+    def to_bytes(self):
+        return self.issuerSpkiHash + self.serial
+
+    def __repr__(self):
+        return f"CertID({self.issuerSpkiHash.hex()}-{self.serial.hex()})"
+
+    def __hash__(self):
+        return hash((self.issuerSpkiHash, self.serial))
+
+    def __eq__(self, other):
+        return self.issuerSpkiHash == other.issuerSpkiHash and self.serial == other.serial
 
 
 def getCertList(certpath, issuer):
-    issuerKey = base64.urlsafe_b64decode(issuer)
+    issuerSpkiHash = base64.urlsafe_b64decode(issuer)
 
     certlist = None
     if os.path.isfile(certpath):
@@ -60,8 +86,8 @@ def getCertList(certpath, issuer):
                 certlist = set()
                 for sHex in serials:
                     try:
-                        key = issuerKey + bytearray.fromhex(sHex)
-                        certlist.add(key)
+                        serial = bytes.fromhex(sHex)
+                        certlist.add(CertId(issuerSpkiHash, serial))
                     except TypeError as te:
                         log.error(f"Couldn't decode issuer={issuer} serial "
                                   + f"hex={sHex} because {te}")
@@ -83,7 +109,7 @@ def initIssuerStats(stats, issuer):
     }
 
 
-def genCertLists(args, stats, *, revoked_certs, nonrevoked_certs):
+def genCertLists(args, stats, *, revoked_certs_by_issuer, nonrevoked_certs_by_issuer):
     stats['knownrevoked'] = 0
     stats['knownnotrevoked'] = 0
     stats['revoked'] = 0
@@ -138,8 +164,13 @@ def genCertLists(args, stats, *, revoked_certs, nonrevoked_certs):
             # cbw - Don't add all revocations, only add revocations
             # for known certificates. Revocations for unknown certs
             # are useless cruft
-            revoked_certs.extend(knownRevoked)
-            nonrevoked_certs.extend(knownNotRevoked)
+            if issuer not in revoked_certs_by_issuer:
+                revoked_certs_by_issuer[issuer] = []
+            revoked_certs_by_issuer[issuer].extend(knownRevoked)
+
+            if issuer not in nonrevoked_certs_by_issuer:
+                nonrevoked_certs_by_issuer[issuer] = []
+            nonrevoked_certs_by_issuer[issuer].extend(knownNotRevoked)
 
     # Go through revoked issuers and process any that were not part of known issuers
     for path, dirs, files in os.walk(args.revokedPath):
@@ -166,30 +197,95 @@ def genCertLists(args, stats, *, revoked_certs, nonrevoked_certs):
                stats['knownrevoked'], stats['nocrl']))
 
 
-def saveCertLists(args, *, revoked_certs, nonrevoked_certs):
+# bytes 0-4: number of serials as an unsigned long
+# bytes 5-6: length of issuer field as a unsigned short
+# bytes 7+: hash of issuer subject public key info
+ISSUERS_FMT = b'<LH'
+issuers_struct = struct.Struct(ISSUERS_FMT)
+
+# bytes 0-1: length of serial field as an unsigned short
+# bytes 2+: ASN.1 DER-encoded serial number
+SERIALS_FMT = b'<H'
+serials_struct = struct.Struct(SERIALS_FMT)
+
+
+def writeCertListByIssuer(file, certs_by_issuer):
+    for issuer_base64 in certs_by_issuer:
+        serial_list = certs_by_issuer[issuer_base64]
+        num_serial_list = len(serial_list)
+
+        issuer = base64.urlsafe_b64decode(issuer_base64)
+        issuer_len = len(issuer)
+
+        if num_serial_list > 0xFFFFFFFF:
+            raise Exception("serial list length > unsigned long")
+        if issuer_len > 0xFFFF:
+            raise Exception("issuer bytes > unsigned short")
+
+        file.write(issuers_struct.pack(num_serial_list, issuer_len))
+        file.write(issuer)
+
+        for k in serial_list:
+            n = len(k.serial)
+            if n > 0xFFFF:
+                raise Exception("serial bytes > unsigned short")
+            file.write(serials_struct.pack(n))
+            file.write(k.serial)
+
+
+class EOFException(Exception):
+    pass
+
+
+def expectRead(file, expectedBytes):
+    result = file.read(expectedBytes)
+    if len(result) == 0:
+        raise EOFException()
+    if len(result) < expectedBytes:
+        breakpoint()
+    return result
+
+
+def readCertListByIssuer(file, certs_by_issuer):
+    try:
+        while True:
+            (num_serial_list, issuer_len) = issuers_struct.unpack(
+                                                expectRead(file, issuers_struct.size))
+            issuer_bytes = expectRead(file, issuer_len)
+
+            issuer = base64.urlsafe_b64encode(issuer_bytes)
+            if issuer not in certs_by_issuer:
+                certs_by_issuer[issuer] = []
+
+            for serial_idx in range(num_serial_list):
+                (serial_len,) = serials_struct.unpack(expectRead(file, serials_struct.size))
+                serial_bytes = expectRead(file, serial_len)
+
+                certs_by_issuer[issuer].append(CertId(issuer_bytes, serial_bytes))
+    except EOFException:
+        pass
+    return
+
+
+def saveCertLists(args, *, revoked_certs_by_issuer, nonrevoked_certs_by_issuer):
     log.info("Saving revoked/nonrevoked list {revoked} {valid}".format(
         revoked=args.revokedKeys, valid=args.validKeys))
     os.makedirs(os.path.dirname(args.revokedKeys), exist_ok=True)
     os.makedirs(os.path.dirname(args.validKeys), exist_ok=True)
-    with open(args.revokedKeys, 'w') as revfile, open(args.validKeys,
-                                                      'w') as nonrevfile:
-        for k in revoked_certs:
-            revfile.write("%s\n" % base64.standard_b64encode(k).decode("utf-8"))
-        for k in nonrevoked_certs:
-            nonrevfile.write("%s\n" % base64.standard_b64encode(k).decode("utf-8"))
+    with open(args.revokedKeys, 'wb') as revfile:
+        writeCertListByIssuer(revfile, revoked_certs_by_issuer)
+    with open(args.validKeys, 'wb') as nonrevfile:
+        writeCertListByIssuer(nonrevfile, nonrevoked_certs_by_issuer)
 
 
-def loadCertLists(args, *, revoked_certs, nonrevoked_certs):
-    log.info("Loading revoked/nonrevoked list {revoked} {valid}".format(
-        revoked=args.revokedKeys, valid=args.validKeys))
-    nonrevoked_certs.clear()
-    revoked_certs.clear()
-    with open(args.revokedKeys, 'r') as file:
-        for line in file:
-            revoked_certs.append(base64.standard_b64decode(line[:-1].encode("utf-8")))
-    with open(args.validKeys, 'r') as file:
-        for line in file:
-            nonrevoked_certs.append(base64.standard_b64decode(line[:-1].encode("utf-8")))
+def loadCertLists(args, *, revoked_certs_by_issuer, nonrevoked_certs_by_issuer):
+    log.info(f"Loading revoked/nonrevoked list {args.revokedKeys} {args.validKeys}")
+    nonrevoked_certs_by_issuer.clear()
+    revoked_certs_by_issuer.clear()
+    with open(args.revokedKeys, 'rb') as file:
+        readCertListByIssuer(file, revoked_certs_by_issuer)
+    with open(args.validKeys, 'rb') as file:
+        readCertListByIssuer(file, nonrevoked_certs_by_issuer)
 
 
 def getFPRs(revoked_certs, nonrevoked_certs):
@@ -286,7 +382,7 @@ def parseArgs(argv):
         help="Exclude the specified Issuers")
     parser.add_argument(
         "-cacheKeys",
-        help="Save revoked/non-revoked sorted certs to file or load from file if it exists.",
+        help="Load revoked/non-revoked sorted certs from cache files.",
         action="store_true")
     parser.add_argument(
         "-noVerify", help="Skip MLBF verification", action="store_true")
@@ -301,8 +397,8 @@ def parseArgs(argv):
     if args.revokedPath is None:
         args.revokedPath = os.path.join(args.certPath, args.id, "revoked")
     args.revokedKeys = os.path.join(args.certPath, args.id,
-                                    args.outDirName, "keys-revoked")
-    args.validKeys = os.path.join(args.certPath, args.id, args.outDirName, "keys-valid")
+                                    args.outDirName, "keys-revoked.keys")
+    args.validKeys = os.path.join(args.certPath, args.id, args.outDirName, "keys-valid.keys")
     return args
 
 
@@ -315,37 +411,57 @@ def saveStats(args, stats):
 
 def main():
     args = parseArgs(sys.argv[1:])
+    log = logging.getLogger('cert_to_crlite')
     log.debug(args)
-    revoked_certs = []
-    nonrevoked_certs = []
+    revoked_certs_by_issuer = {}
+    nonrevoked_certs_by_issuer = {}
 
     stats = {}
 
     sw.start('crlite')
-    sw.start('certs')
-    if args.cacheKeys is True and os.path.isfile(
-            args.revokedKeys) and os.path.isfile(args.validKeys):
+    if args.cacheKeys is True:
+        if not (os.path.isfile(args.revokedKeys) and os.path.isfile(args.validKeys)):
+            raise Exception(f"Could not load cacheKeys from {args.revokedKeys}"
+                            + f" or {args.validKeys}")
+        sw.start('load certs')
+
         loadCertLists(
             args,
-            revoked_certs=revoked_certs,
-            nonrevoked_certs=nonrevoked_certs)
+            revoked_certs_by_issuer=revoked_certs_by_issuer,
+            nonrevoked_certs_by_issuer=nonrevoked_certs_by_issuer)
+        sw.end('load certs')
     else:
+        sw.start('certs')
+        sw.start('gen certs')
         genCertLists(
             args,
             stats,
-            revoked_certs=revoked_certs,
-            nonrevoked_certs=nonrevoked_certs)
-        if args.cacheKeys is True:
-            saveCertLists(
-                args,
-                revoked_certs=revoked_certs,
-                nonrevoked_certs=nonrevoked_certs)
-    log.debug(
-        "Cert lists revoked/non-revoked R: {revoked} NR: {nonrevoked}".format(
-            revoked=len(revoked_certs), nonrevoked=len(nonrevoked_certs)))
-    sw.end('certs')
+            revoked_certs_by_issuer=revoked_certs_by_issuer,
+            nonrevoked_certs_by_issuer=nonrevoked_certs_by_issuer)
+        sw.end('gen certs')
+        sw.start('save certs')
+        saveCertLists(
+            args,
+            revoked_certs_by_issuer=revoked_certs_by_issuer,
+            nonrevoked_certs_by_issuer=nonrevoked_certs_by_issuer)
+        sw.end('save certs')
+        sw.end('certs')
 
-    if len(revoked_certs) == 0 or len(nonrevoked_certs) == 0:
+    revoked_certs = []
+    [revoked_certs.extend(d) for d in revoked_certs_by_issuer.values()]
+    revoked_certs_by_issuer = {}  # allow garbage collection
+
+    nonrevoked_certs = []
+    [nonrevoked_certs.extend(d) for d in nonrevoked_certs_by_issuer.values()]
+    nonrevoked_certs_by_issuer = {}  # allow garbage collection
+
+    num_revoked_certs = len(revoked_certs)
+    num_nonrevoked_certs = len(nonrevoked_certs)
+
+    log.debug(f"Cert lists revoked/non-revoked R: {num_revoked_certs} "
+              + f"NR: {num_nonrevoked_certs}")
+
+    if num_revoked_certs == 0 and num_nonrevoked_certs == 0:
         log.info("No certificates, exiting failure")
         sys.exit(1)
 
@@ -378,5 +494,4 @@ def main():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    log = logging.getLogger('cert_to_crlite')
     main()
---
+++ create_filter_cascade/test.py
@@ -0,0 +1,92 @@
+import certs_to_crlite
+import unittest
+
+from pathlib import Path
+import argparse
+import base64
+import tempfile
+
+
+class MockFile(object):
+    def __init__(self):
+        self.data = b""
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+    def write(self, s):
+        self.data = self.data + s
+
+    def read(self):
+        return self.data
+
+    def flush(self):
+        pass
+
+
+def make_certid(issuer, hex):
+    return certs_to_crlite.CertId(base64.urlsafe_b64decode(issuer), bytes.fromhex(hex))
+
+
+def static_test_certs():
+    revoked = {
+        b"aG9uZXN0Q0EK": [
+            make_certid("aG9uZXN0Q0EK", "00AA"),
+            make_certid("aG9uZXN0Q0EK", "AA00"),
+        ],
+        b"b3RoZXJDQQo=": [
+            make_certid("b3RoZXJDQQo=", "FFCCDD")
+        ],
+    }
+    nonrevoked = {
+        b"aG9uZXN0Q0EK": [
+            make_certid("aG9uZXN0Q0EK", "AAAAAA"),
+            make_certid("aG9uZXN0Q0EK", "000000"),
+        ],
+        b"dGhpcmRDQQo=": [
+            make_certid("dGhpcmRDQQo=", "CACACA"),
+        ],
+    }
+    return revoked, nonrevoked
+
+
+class TestCertLists(unittest.TestCase):
+    def assertCertListEqual(self, a, b):
+        self.assertEqual(len(a), len(b))
+
+        for issuer in a:
+            s_a = set(a[issuer])
+            s_b = set(b[issuer])
+            self.assertEqual(s_a, s_b)
+
+    def test_save_and_load(self):
+        revoked, nonrevoked = static_test_certs()
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            args = argparse.Namespace()
+            args.revokedKeys = tmpdirname / Path("revoked.bin")
+            args.validKeys = tmpdirname / Path("valid.bin")
+
+            certs_to_crlite.saveCertLists(args,
+                                          revoked_certs_by_issuer=revoked,
+                                          nonrevoked_certs_by_issuer=nonrevoked)
+
+            self.assertEqual(Path(args.revokedKeys).stat().st_size, 42)
+            self.assertEqual(Path(args.validKeys).stat().st_size, 44)
+
+            loaded_revoked = {}
+            loaded_nonrevoked = {}
+
+            certs_to_crlite.loadCertLists(args,
+                                          revoked_certs_by_issuer=loaded_revoked,
+                                          nonrevoked_certs_by_issuer=loaded_nonrevoked)
+
+            self.assertCertListEqual(loaded_revoked, revoked)
+            self.assertCertListEqual(loaded_nonrevoked, nonrevoked)
+
+
+if __name__ == '__main__':
+    unittest.main()
