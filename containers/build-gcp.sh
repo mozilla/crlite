e2fd9c9232398c4113c68c561ffb33254176feed
J.C. Jones <jc@mozilla.com>
2020-06-22 13:23:16 -0700

    Automatically update deployments in GKE

--- containers/build-gcp.sh
+++ containers/build-gcp.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -xe
+TAG=testing
+
 cd $(dirname $0)
 gcloud config set project ${crlite_project:-crlite-beta}
-gcloud builds submit --config ./cloudbuild.yaml ..
+
+gcloud builds submit --config ./cloudbuild.yaml --substitutions _CRLITE_TAG=${TAG} ..
+
+kubectl set image deployment/crlite-fetch crlite-fetch=gcr.io/crlite-beta/crlite:${TAG}-fetch
+kubectl set image cronjob/crlite-generate crlite-generate=gcr.io/crlite-beta/crlite:${TAG}-generate
+kubectl set image cronjob/crlite-publish crlite-publish=gcr.io/crlite-beta/crlite:${TAG}-publish
