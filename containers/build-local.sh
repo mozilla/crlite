2f4354aa8f2849a86557e014df30f8b366bb2745
J.C. Jones <jc@mozilla.com>
2020-06-22 17:06:55 -0700

    Use ct-mapreduce 1.0.7 that adds the health check

--- containers/build-local.sh
+++ containers/build-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 VER=staging
-CT_MAPREDUCE_VER=v1.0.6
+CT_MAPREDUCE_VER=v1.0.7
 
 docker build -t crlite:${VER} .. -f Dockerfile --build-arg ct_mapreduce_ver=${CT_MAPREDUCE_VER}
 docker build -t crlite:${VER}-fetch .. -f crlite-fetch/Dockerfile --build-arg crlite_image=crlite:${VER}
