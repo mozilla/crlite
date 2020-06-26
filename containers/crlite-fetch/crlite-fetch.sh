07d911dd751a3bb288ef0237159dd3de75c7734f
J.C. Jones <jc@mozilla.com>
2020-06-26 16:13:34 -0700

    Adjust the logging levels for ct-fetch

--- containers/crlite-fetch/crlite-fetch.sh
+++ containers/crlite-fetch/crlite-fetch.sh
@@ -4,15 +4,8 @@ workflow=${crlite_workflow:-~/go/src/github.com/mozilla/crlite/workflow}
 
 source ${workflow}/0-set_credentials.inc
 
-cmd="${crlite_bin:-~/go/bin}/ct-fetch -log_dir ${crlite_log:-/tmp}/ -stderrthreshold=WARNING"
-
-echo "Starting: ${cmd}"
 echo "Expect ${outputRefreshPeriod} lag for initial output."
 
-if [ -x /usr/bin/time ] ; then
-  /usr/bin/time ${cmd}
-else
-  ${cmd}
-fi
+${crlite_bin:-~/go/bin}/ct-fetch -nobars -logtostderr -stderrthreshold=INFO
 
 exit 0
