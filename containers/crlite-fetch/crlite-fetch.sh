4eaece88b9617e646a594e08e92779bf2e7f074e
J.C. Jones <jc@mozilla.com>
2020-07-01 08:22:01 -0700

    Don't require credentials for ct-fetch (#100)

--- containers/crlite-fetch/crlite-fetch.sh
+++ containers/crlite-fetch/crlite-fetch.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e
 
-workflow=${crlite_workflow:-~/go/src/github.com/mozilla/crlite/workflow}
-
-source ${workflow}/0-set_credentials.inc
-
-echo "Expect ${outputRefreshPeriod} lag for initial output."
-
 ${crlite_bin:-~/go/bin}/ct-fetch -nobars -logtostderr -stderrthreshold=INFO
 
 exit 0
