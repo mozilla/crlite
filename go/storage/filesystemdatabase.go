686279262d1a818883719f76fdd2ef6b8cd01440
Dana Keeler <dkeeler@mozilla.com>
2021-04-28 15:56:03 -0700

    remove unused StorageBackend from FilesystemDatabase

--- go/storage/filesystemdatabase.go
+++ go/storage/filesystemdatabase.go
@@ -1,15 +1,10 @@
 package storage
 
 import (
-	"context"
-	"crypto/sha1"
-	"encoding/pem"
 	"fmt"
 	"net/url"
-	"strconv"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/bluele/gcache"
 	"github.com/golang/glog"
@@ -17,17 +12,14 @@ import (
 )
 
 type FilesystemDatabase struct {
-	backend         StorageBackend
 	extCache        RemoteCache
 	knownCertsCache gcache.Cache
 	metaMutex       *sync.RWMutex
 	meta            map[string]*IssuerMetadata
 }
 
-func NewFilesystemDatabase(aBackend StorageBackend, aExtCache RemoteCache) (*FilesystemDatabase,
-	error) {
+func NewFilesystemDatabase(aExtCache RemoteCache) (*FilesystemDatabase, error) {
 	db := &FilesystemDatabase{
-		backend:         aBackend,
 		extCache:        aExtCache,
 		knownCertsCache: gcache.New(8 * 1024).ARC().Build(),
 		metaMutex:       &sync.RWMutex{},
@@ -99,27 +91,11 @@ func (db *FilesystemDatabase) GetIssuerAndDatesFromCache() ([]IssuerDate, error)
 	return issuerList, nil
 }
 
-func (db *FilesystemDatabase) ListExpirationDates(aNotBefore time.Time) ([]ExpDate, error) {
-	return db.backend.ListExpirationDates(context.Background(), aNotBefore)
-}
-
-func (db *FilesystemDatabase) ListIssuersForExpirationDate(expDate ExpDate) ([]Issuer, error) {
-	return db.backend.ListIssuersForExpirationDate(context.Background(), expDate)
-}
-
 func (db *FilesystemDatabase) SaveLogState(aLogObj *CertificateLog) error {
-	ctx, ctxCancel := context.WithCancel(context.Background())
-	defer ctxCancel()
-	err := db.extCache.StoreLogState(aLogObj)
-	if err != nil {
-		glog.Warningf("Couldn't store log state for %s: %s", aLogObj, err)
-	}
-	return db.backend.StoreLogState(ctx, aLogObj)
+	return db.extCache.StoreLogState(aLogObj)
 }
 
 func (db *FilesystemDatabase) GetLogState(aUrl *url.URL) (*CertificateLog, error) {
-	ctx, ctxCancel := context.WithCancel(context.Background())
-	defer ctxCancel()
 	shortUrl := fmt.Sprintf("%s%s", aUrl.Host, aUrl.Path)
 
 	log, cacheErr := db.extCache.LoadLogState(shortUrl)
@@ -127,32 +103,10 @@ func (db *FilesystemDatabase) GetLogState(aUrl *url.URL) (*CertificateLog, error
 		return log, cacheErr
 	}
 
-	log, backendErr := db.backend.LoadLogState(ctx, shortUrl)
-	if log != nil {
-		return log, backendErr
-	}
-
-	glog.Warningf("Allocating brand new log for %+v, cache err=%v, backend err=%v", shortUrl, cacheErr, backendErr)
+	glog.Warningf("Allocating brand new log for %+v, cache err=%v", shortUrl, cacheErr)
 	return &CertificateLog{
 		ShortURL: shortUrl,
 	}, nil
-}
-
-func (db *FilesystemDatabase) markDirty(aExpiration *time.Time) error {
-	subdirName := aExpiration.Format(kExpirationFormat)
-	return db.backend.MarkDirty(subdirName)
-}
-
-func getSpki(aCert *x509.Certificate) SPKI {
-	if len(aCert.SubjectKeyId) < 8 {
-		digest := sha1.Sum(aCert.RawSubjectPublicKeyInfo)
-
-		glog.V(2).Infof("[issuer: %s] SPKI is short: %v, using %v instead.",
-			aCert.Issuer.String(), aCert.SubjectKeyId, digest[0:])
-		return SPKI{digest[0:]}
-	}
-
-	return SPKI{aCert.SubjectKeyId}
 }
 
 func (db *FilesystemDatabase) Store(aCert *x509.Certificate, aIssuer *x509.Certificate,
@@ -161,53 +115,11 @@ func (db *FilesystemDatabase) Store(aCert *x509.Certificate, aIssuer *x509.Certi
 	issuer := NewIssuer(aIssuer)
 	knownCerts := db.GetKnownCertificates(expDate, issuer)
 
-	ctx, ctxCancel := context.WithCancel(context.Background())
-	defer ctxCancel()
-
-	headers := make(map[string]string)
-	headers["Log"] = aLogURL
-	headers["Recorded-at"] = time.Now().Format(time.RFC3339)
-	headers["Entry-id"] = strconv.FormatInt(aEntryId, 10)
-	pemblock := pem.Block{
-		Type:    "CERTIFICATE",
-		Headers: headers,
-		Bytes:   aCert.Raw,
-	}
-
 	serialNum := NewSerial(aCert)
 
-	certWasUnknown, err := knownCerts.WasUnknown(serialNum)
-	if err != nil {
-		return err
-	}
-
-	if certWasUnknown {
-		issuerDateSeenBefore, err := db.GetIssuerMetadata(issuer).Accumulate(aCert)
-		if err != nil {
-			return err
-		}
-		if !issuerDateSeenBefore {
-			// if the issuer/expdate was unknown in the cache
-			errAlloc := db.backend.AllocateExpDateAndIssuer(ctx, expDate, issuer)
-			if errAlloc != nil {
-				return errAlloc
-			}
-		}
-
-		errStore := db.backend.StoreCertificatePEM(ctx, serialNum, expDate, issuer,
-			pem.EncodeToMemory(&pemblock))
-		if errStore != nil {
-			return errStore
-		}
-	}
-
-	// Mark the directory dirty
-	err = db.markDirty(&aCert.NotAfter)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	// WasUnknown stores the certificate if it was unknown.
+	_, err := knownCerts.WasUnknown(serialNum)
+	return err
 }
 
 func (db *FilesystemDatabase) GetKnownCertificates(aExpDate ExpDate,
@@ -237,9 +149,4 @@ func (db *FilesystemDatabase) GetKnownCertificates(aExpDate ExpDate,
 		panic("kc is null")
 	}
 	return kc
-}
-
-func (db *FilesystemDatabase) Cleanup() error {
-	// TODO: Remove
-	return nil
 }
