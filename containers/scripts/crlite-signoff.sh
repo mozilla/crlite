f77b130626e74a4c44500161f5990fce3eac357c
John M. Schanck <jschanck@mozilla.com>
2024-10-11 16:15:55 -0700

    crlite-signoff: move filter size check

--- containers/scripts/crlite-signoff.sh
+++ containers/scripts/crlite-signoff.sh
@@ -23,6 +23,12 @@ fi
 # sign off on intermediates
 python3 "${SCRIPTS}/crlite-signoff-tool.py" intermediates
 
+if ! "${RUST_QUERY_CRLITE}" -vvv --db "${OUTPUT}" --update "${INSTANCE}" signoff "${crlite_verify_host_file_urls}"
+then
+  echo "Verification domains test failed"
+  exit 1;
+fi
+
 # sign off on cert-revocations if the verification domains test passes and the
 # filter is less than MAX_FILTER_SIZE.
 FILTER_SIZE=$(stat --format=%s "${OUTPUT}/crlite.filter")
@@ -31,12 +37,6 @@ echo "Filter is ${FILTER_SIZE} bytes."
 if (( FILTER_SIZE > MAX_FILTER_SIZE ))
 then
   echo "Cannot automatically sign off on a filter larger than max_filter_size=${MAX_FILTER_SIZE} bytes."
-  exit 1;
-fi
-
-if ! "${RUST_QUERY_CRLITE}" -vvv --db "${OUTPUT}" --update "${INSTANCE}" signoff "${crlite_verify_host_file_urls}"
-then
-  echo "Verification domains test failed"
   exit 1;
 fi
 
