b1ddbc5f10019fa3d96b1c867d2e9fa574ac3fba
John M. Schanck <jschanck@mozilla.com>
2026-05-11 08:37:36 -0700

    crlite-signoff: stop enforcing verification_domains.txt check.

--- containers/scripts/crlite-signoff.sh
+++ containers/scripts/crlite-signoff.sh
@@ -23,20 +23,13 @@ fi
 # sign off on intermediates
 python3 "${SCRIPTS}/crlite-signoff-tool.py" intermediates
 
-# sign off on cert-revocations if the verification domains test passes and the
-# filter is less than MAX_FILTER_SIZE.
+# sign off on cert-revocations if the filter is less than MAX_FILTER_SIZE.
 FILTER_SIZE=$(stat --format=%s "${OUTPUT}/crlite.filter")
 echo "Filter is ${FILTER_SIZE} bytes."
 
 if (( FILTER_SIZE > MAX_FILTER_SIZE ))
 then
   echo "Cannot automatically sign off on a filter larger than max_filter_size=${MAX_FILTER_SIZE} bytes."
-  exit 1;
-fi
-
-if ! "${RUST_QUERY_CRLITE}" -v --db "${OUTPUT}" --update "${INSTANCE}" signoff "${crlite_verify_host_file_urls}"
-then
-  echo "Verification domains test failed"
   exit 1;
 fi
 
